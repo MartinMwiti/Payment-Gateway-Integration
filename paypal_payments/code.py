886c2cae661ba5d239ef96f61234b9fe5898e734
MartinMwiti <martinmwiti007@gmail.com>
2020-04-21 16:39:31 +0300

    unfinished

---
+++ paypal_payments/code.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+
+    return jsonify({'paymentID': payment.id})
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
